86b9fe48bd7e452b083c894e56b36723b645195e
thong.dinh <datthong180997@gmail.com>
2023-02-10 15:52:23 +0700

    add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AngularFireModule} from "@angular/fire/compat";
+import {environment} from "../environments/environment";
+import {AngularFireMessagingModule} from "@angular/fire/compat/messaging";
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    // Firebase
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireMessagingModule,
+    // App
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    // Material
+    MatToolbarModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDpVtZOHGGkQPqHXVpFtUQDOOYHDlxjWvo",
+    authDomain: "san-trong.firebaseapp.com",
+    projectId: "san-trong",
+    storageBucket: "san-trong.appspot.com",
+    messagingSenderId: "705712856935",
+    appId: "1:705712856935:web:755ca58297d64183c559c2"
+  }
+};
